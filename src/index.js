191eb18d3fc5209421e978b3e70f8c3b81dccf1b
ankopylova <akopylova.excessteam@gmail.com>
2020-12-08 16:45:07 +0300

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        }
+        this.increaseValue = this.increaseValue.bind(this);
+        this.decreaseValue = this.decreaseValue.bind(this);
+    }
+
+    increaseValue = () => {
+        this.setState({counter: ++this.state.counter})
+    }
+
+    decreaseValue = () => {
+        this.setState({counter: --this.state.counter})
+    }
+
+    render() {
+        const {counter} = this.state;
+        return (
+            <div className='count'>
+                <p>{counter}</p>
+                <button onClick={this.decreaseValue} className={'minus'}>-</button>
+                <button onClick={this.increaseValue} className={'plus'}>+</button>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Counter/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
